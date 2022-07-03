f864830d16a49d6b935b2e6cfdfdf19d7ae3fbbb
dezhizhang <1541609448@qq.com>
2022-07-03 19:40:16 +0800

    feat:jsx

--- docs/plugin-transform-react-jsx.js
+++ docs/plugin-transform-react-jsx.js
@@ -13,8 +13,52 @@ const pluginTransformReactJsx = {
 };
 
 function buildJSXElementCall(path) {
-  const args = [];
+  const openingElementPath = path.get('openingElement');
+  const { name } = openingElementPath.node.name;
+  const tag = types.stringLiteral(name);
+  const args = [tag];
+  const attributes = [];
+  for (let attrPath of openingElementPath.get('attributes')) {
+    attributes.push(attrPath.node);
+  }
+  const props = attributes.map(convertAttribute);
+  const children = buildChildren(path.node);
+  if (children.length > 0) {
+    props.push(buildChildrenProperty(children));
+  }
+
+  const propsObject = types.objectExpression(props);
+  args.push(propsObject);
+
+  //   console.log('props',props);
   return call(path, 'jsx', args);
+}
+
+function convertAttribute(node) {
+  const value = node.value;
+  node.name.type = 'Identifier';
+  const objectProperty = types.objectProperty(node.name, value);
+  return objectProperty;
+}
+
+function buildChildren(node) {
+  let elements = [];
+  for (let i = 0; i < node.children.length; i++) {
+    let child = node.children[i];
+    elements.push(types.stringLiteral(child.value));
+  }
+  return elements;
+}
+
+function buildChildrenProperty(children) {
+  let childrenNode;
+  if (children.length === 1) {
+    (childrenNode = children[0]);
+  } else if (children.length > 1) {
+     (childrenNode = types.arrayExpression(children));
+  }
+  return types.objectProperty(types.identifier('children'),childrenNode);
+
 }
 
 function call(path, name, args) {
